8103ef7ad57daa4ec2ca615a2a44ed5eceefbeb6
death37 <biggles414@gmail.com>
2019-02-06 20:22:27 +0100

    unknow

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -1,8 +1,11 @@
 require('../scss/app.scss');
 require('./jquery-3.3.1.min');
 require('./jquery-ui');
-//require('./jquery.ui.widget');
-require('../js/jquery.collection');
-require('./tickets');
 require('./materialize');
+//require('./jquery.ui.widget');
+require('moment');
+require('../js/jquery.collection');
+require('./datatable-material');
+require('./tickets');
+
 
--- web/assets/js/tickets.js
+++ web/assets/js/tickets.js
@@ -2,5 +2,23 @@ $(document).ready(function () {
     $('.my-selector').collection({
     });
     $('select').formSelect();
-});
+    
+//    $.fn.dataTable.moment('DD-MMM-YYYY');
+    $('#table_id').DataTable({
+        language: {
+            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"},
+        columnDefs: [ {
+            "targets": 3,
+                       "render" : function(data){return moment(data).format('DD-MM-YYYY');}
+            },
+         ],
+        responsive: true,
+        select: true,
+        retrieve: true,
+        paging: false,
+        "ordering": false,
+        destroy: true,
+        dom: "frtip"
 
+    });
+});
